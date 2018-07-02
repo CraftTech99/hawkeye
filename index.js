5e21491f357aa728971b46d0722269b5519b2093
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 19:07:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ client.on("message", (message) => {
   
    if (/message.content.startsWith(prefix)) return;
   
-  if (message.content.startsWith(prefix + "/help")) {
+  if (message.content.startsWith(prefix + "help")) {
     message.channel.send("type /invite to get the invite link of this server~");
   } else
 
-  if (message.content.startsWith(prefix + "/invite")) {
+  if (message.content.startsWith(prefix + "invite")) {
     message.channel.send("https://discord.gg/zPZJah");
   }
 });
