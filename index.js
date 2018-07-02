659ac391c1bc5c98ee649ec6b17ed5e9ef5d0db6
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 09:23:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const Discord = require ("discord.js")
+const bot = new Discord.Client
 
+client.on ("ready", () => {
+  console.log ("I Am Ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
