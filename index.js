708c30736469c077af8fa8e6da6efb51dc613dd3
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 20:12:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,14 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.config = require('botconfig.json');
-
-client.on('ready', () => {
-    client.user.setActivity('https://github.com/CraftTech99/hawkeye', {type: 'Official PlatinumCraft Bot'});
-});
-
-
 client.on("message", (message) => {
   
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  if (message.content.startsWith("help")) {
+  if (message.content.startsWith("/help")) {
     message.channel.send("type /invite to get the invite link of this server~");
-  }
+  } else
 
-  if (message.content.startsWith("invite")) {
-    message.channel.send("https://discord.gg/qtqE7uZ");
-  }
-  
-  if (message.content.startsWith("invite link pls")) {
-    message.channel.send("https://discord.gg/qtqE7uZ");
+  if (message.content.startsWith("/invite")) {
+    message.channel.send("https://discord.gg/zPZJah");
   }
 });
 
