c68f25076cf0d92c9da9238fc75ea3b4512a49db
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 19:41:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const Discord = require('discord.js');const client = new Discord.Client();
 client.on('ready', () => {  
   console.log('I am ready!');
 });
+
+
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'Official PlatinumCraft Bot'});
+    client.user.setActivity('https://github.com/CraftTech99/hawkeye', {type: 'Official PlatinumCraft Bot'});
 });
+
+
 client.on("message", (message) => {
   if (/msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-  if (message.content.startsWith("help")) {
+  if (message.content.startsWith(PREFIX + "help")) {
     message.channel.send("type /invite to get the invite link of this server~");
-  } else
+  }
 
-  if (message.content.startsWith("invite")) {
+  if (message.content.startsWith(PREFIX + "invite")) {
     message.channel.send("https://discord.gg/qtqE7uZ");
   }
   
