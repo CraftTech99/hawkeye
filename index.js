c63282a23994d91e0cfafdec516cf49d2859882c
CraftTech99 <40752080+CraftTech99@users.noreply.github.com>
2018-07-02 19:38:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'Official PlatinumCraft Bot'});
 });
 client.on("message", (message) => {
-  if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-  if (message.content.startsWith("/help")) {
+  if (/msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+  if (message.content.startsWith("help")) {
     message.channel.send("type /invite to get the invite link of this server~");
   } else
 
-  if (message.content.startsWith("/invite")) {
+  if (message.content.startsWith("invite")) {
     message.channel.send("https://discord.gg/qtqE7uZ");
   }
+  
+  });
+  client.on("message", (message) => {
   if (message.content.startsWith("invite link pls")) {
     message.channel.send("https://discord.gg/qtqE7uZ");
   }
